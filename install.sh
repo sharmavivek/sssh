d9c3fd9a9732f3dab44bc6fc4e13f0e8680cbf61
Vivek Sharma <vks2108@gmail.com>
2013-09-16 07:44:34 +0530

    how to install started

--- install.sh
+++ install.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# Get the latest file from github
 wget $file_name
-chmod + $__file
-mkdir -p /home/`$whoami`/.sssh_profile
-mkdir -p /home/`$whoami`/.sssh_history
-./$__file
 
+# Change the permisison to file to exe
+chmod + $__file
+
+# Creating conf files
+mkdir -p /home/`$whoami`/.config/sssh
+mkdir -p /home/`$whoami`/.config/sssh
+
+# Put default entries in the above file.
+echo "" > /home/`$whoami`/.config/sssh/sssh_profile
+echo "" > /home/`$whoami`/.config/sssh/sssh_history
+
+
+# Running the installer
+# 
+./$__file
+mv $__file /usr/bin
 rm
+
+
+#/home/`$whoami`/.local/share/Trash
--- sssh_manual.md
+++ sssh_manual.md
@@ -1,3 +1,31 @@
-Manual for Simplified SSH
-- parallel approach
-- The command just runs prints the usage, and then ask for input
+# manual sssh
+----
+
+<br>  
+## Install 
+1. wget 
+2. chmod -x file_name
+3. 
+4. 
+
+<br>
+## Usage
+##### Example 1
+
+##### Example 2
+
+##### Example 3
+
+##### Example 4  
+  
+<!---
+comments goes in here
+use <br> for giving an empty line
+-->
+<br>
+
+
+## To Do
+1. encrypted password
+2. automation from ssh profiles
+3. 
